a8ec9811e46a64246ff5a0bdc70dfad2ee6f2f29
Shelby Talbert <mi.shelbyrose@gmail.com>
2020-08-31 23:38:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,8 +2,8 @@
 def game_hash
   {
     home: {
-      :team_name => "Brooklyn Nets"
-      :colors => ["Black", "White"]
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
       :players => [
         {player_name:
           number:
@@ -58,8 +58,8 @@ def game_hash
         ],
       }
   away: {
-    :team_name => "Charlotte Hornets"
-    :colors  => ["Turquoise", "Purple"]
+    :team_name => "Charlotte Hornets",
+    :colors  => ["Turquoise", "Purple"],
     :players => [
       {player_name:
         number:
